19e78a80a63236ad37d17b3f649296e81b33053c
Andre Moeller <13366062+andregmoeller@users.noreply.github.com>
2020-02-23 20:01:33 +0100

    Searches for characters

--- sites/lookup/src/pages/search.js
+++ sites/lookup/src/pages/search.js
@@ -1,12 +1,28 @@
 import React from 'react';
+import Form from '../components/form';
+import Results from '../components/results';
 
-const Index = () => (
-  <>
-    <h1>Search for Rick & Morty Characters</h1>
-    <p>
-      Trying to remember which Rick you’re talking about? Try out this handy search interface!
-    </p>
-  </>
-);
+const Search = ({ location }) => {
+const query = 
+  (location.state && location.state.query ) ||
+  location.pathname.replace(/^\/search\/?/, '') ||
+  '';
+  const name = query.replace(/-+/g, ' ');
 
-export default Index;
+return (
+    <>
+      <h1>
+        {name
+          ? `Showing results for “${name}”`
+          : 'Search for Rick and Morty Characters'}
+      </h1>
+      <p>
+        Trying to remember which Rick you’re talking about? Try out this handy search interface!
+      </p>
+      <Form />
+      {name && <Results name={name} />}
+    </>
+  )
+};
+
+export default Search;
