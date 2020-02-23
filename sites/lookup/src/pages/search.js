c1abc4d5736371c09c8dfd2c69ce3a3f19fe7ed9
Andre Moeller <13366062+andregmoeller@users.noreply.github.com>
2020-02-23 18:56:01 +0100

    Sets up netlify redirects

---
+++ sites/lookup/src/pages/search.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Index = () => (
+  <>
+    <h1>Search for Rick & Morty Characters</h1>
+    <p>
+      Trying to remember which Rick you’re talking about? Try out this handy search interface!
+    </p>
+  </>
+);
+
+export default Index;
